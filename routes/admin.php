1b5c76df27bd51105e96505b23a3a1d85d352987
LinhCtyPc <linhvh@vietis.com.vn>
2022-05-20 15:43:20 +0700

    add Admin/Logout

--- routes/admin.php
+++ routes/admin.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::match(['get', 'post'], '/login', [LoginController::class, 'login'])->name('admin.login');
 Route::middleware('auth:admin')->group(function (){
+    Route::get('/logout', 'App\Http\Controllers\Admin\Auth\LogoutController@perform')->name('logout.perform');
     Route::get('/dashboard', 'App\Http\Controllers\Admin\DashboardController@index')->name('dashboard');
     Route::resource('/manager-product','App\Http\Controllers\Admin\ProductController');
     Route::resource('/manager-order','App\Http\Controllers\Admin\OrderController');
